f12db226d554346fd8ce56e4ebedb8e329d3d05c
huntourinho <32179596+huntourinho@users.noreply.github.com>
2017-12-17 12:02:27 +0100

    Add files via upload

---
+++ 3DSCTF_2017/img2file.py
@@ -0,0 +1,16 @@
+import re	
+import binascii
+import requests
+r=requests.get("http://bitmap01.3dsctf.org:8010/")
+allcolors = re.search(r'<tr>(.*)</tr>',r.text)
+allcolors=allcolors.group(1)
+list=re.findall('"\#([^"]*)"', r.text)
+with open('flag', 'wb') as program:
+	for l in list:
+		program.write(binascii.unhexlify(l))
+program.close()
+
+#root@Scorp:~# python img2file.py
+#root@Scorp:~# chmod +x flag && ./flag
+#3DS{H1dd3n_1n_7ru3_C0l0r5}
+
